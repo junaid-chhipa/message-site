7d6179bf4069df9e4690e3525a1b1aa5a653b8b4
junaid-chhipa <juchhipa@gmail.com>
2025-04-15 00:02:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+const messageWrapper = document.getElementById("messageWrapper");
 const messageContainer = document.getElementById("messageContainer");
 const messageBtn = document.getElementById("messageBtn");
 const closeBtn = document.getElementById("closeBtn");
 
 messageBtn.addEventListener("click", () => {
-  messageContainer.classList.remove("hidden");
-  messageContainer.classList.add("visible");
+  messageWrapper.classList.remove("hidden");
+  messageWrapper.classList.add("visible");
 });
 
 closeBtn.addEventListener("click", () => {
-  messageContainer.classList.remove("visible");
+  messageWrapper.classList.remove("visible");
   setTimeout(() => {
-    messageContainer.classList.add("hidden");
-  }, 500); // match CSS transition
+    messageWrapper.classList.add("hidden");
+  }, 500);
 });
